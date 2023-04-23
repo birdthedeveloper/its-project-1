22789a6906c993fed2fd23c60171fea1c653606c
Martin Ptáček <martin.ptace@gmail.com>
2023-04-23 23:47:13 +0200

    refactor

--- features/steps/order_management.py
+++ features/steps/order_management.py
@@ -2,45 +2,36 @@ from behave import *
 
 @given("Admin is logged in to dashboard")
 def step_impl(context):
-    # implementation code
     pass
 
 @when("Admin navigates to orders")
 def step_impl(context):
-    # implementation code
     pass
 
 @then("Admin can list all orders in the system")
 def step_impl(context):
-    # implementation code
     pass
 
 @given("Admin is logged in to orders")
 def step_impl(context):
-    # implementation code
     pass
 
 @when("Admin fills out necessary data for new order and saves it")
 def step_impl(context):
-    # implementation code
     pass
 
 @then("New order is present in the system")
 def step_impl(context):
-    # implementation code
     pass
 
 @given("One order exists in the system")
 def step_impl(context):
-    # implementation code
     pass
 
 @when("Admin changes state of the order to complete")
 def step_impl(context):
-    # implementation code
     pass
 
 @then("Order history log records this change")
 def step_impl(context):
-    # implementation code
     pass
--- features/steps/product_management.py
+++ features/steps/product_management.py
@@ -2,15 +2,12 @@ from behave import *
 
 @given("Admin is logged in to products")
 def step_impl(context):
-    # implementation code
     pass
 
 @when("Admin fills out necessary information for new product")
 def step_impl(context):
-    # implementation code
     pass
 
 @then("New product is present in products page")
 def step_impl(context):
-    # implementation code
     pass
